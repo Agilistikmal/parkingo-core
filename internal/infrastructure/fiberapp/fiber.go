68cb9d0bc8f857105a48a0fe927ba8efc72d31e8
agilistikmal <agilistikmal3@gmail.com>
2025-03-11 07:55:53 +0700

    cors

--- internal/infrastructure/fiberapp/fiber.go
+++ internal/infrastructure/fiberapp/fiber.go
@@ -1,9 +1,19 @@
 package fiberapp
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func NewFiberApp() *fiber.App {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,PATCH,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+	}))
 
 	return app
 }
