128a4ab8c08bca3dd8c527599e38fe78fe2e4c67
agilistikmal <agilistikmal3@gmail.com>
2025-04-28 14:14:31 +0700

    change cors back to port 3000

--- internal/infrastructure/fiberapp/fiber.go
+++ internal/infrastructure/fiberapp/fiber.go
@@ -9,7 +9,7 @@ func NewFiberApp() *fiber.App {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://parkingo.agil.zip,http://localhost:5000",
+		AllowOrigins:     "https://parkingo.agil.zip,http://localhost:3000",
 		AllowMethods:     "GET,POST,PUT,DELETE,PATCH,OPTIONS",
 		AllowHeaders:     "Authorization, Content-Type, X-Callback-Token",
 		AllowCredentials: true,
