3cfc04bbe1d2a05a4c23668aea4c749573dc623c
agilistikmal <agilistikmal3@gmail.com>
2025-05-29 15:58:56 +0700

    handle duplicate slug

--- internal/app/services/parking_service.go
+++ internal/app/services/parking_service.go
@@ -161,7 +161,10 @@ func (s *ParkingService) UpdateParking(id int, req *models.UpdateParkingRequest)
 	if req.Slug != "" {
 		var existingParking models.Parking
 		err = s.DB.First(&existingParking, "slug = ?", req.Slug).Error
-		if err == nil {
+		if err != nil {
+			return nil, err
+		}
+		if existingParking.ID != parking.ID {
 			return nil, errors.New("slug already exists")
 		}
 		parking.Slug = req.Slug
